65d7866614827b5c7b33c2473666076eb326e9ef
Waleed Ahmad <waleedgplus@gmail.com>
2016-10-12 16:15:34 +0500

    added auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,35 +1,38 @@
 <?php
 
-Route::group(['middleware' => ['web']], function () {
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
 
-    Route::get('/', 'DashboardController@index');
+    Route::group(['middleware'  =>  'auth'], function(){
+        Route::get('/', 'DashboardController@index');
 
-    Route::get('/warehouse/{id}/products', 'WareHouseController@index');
-    Route::get('/warehouse/edit/{id}', 'WareHouseController@editWareHouse');
-    Route::post('/warehouse/update', 'WareHouseController@updateWarehouse');
-    Route::get('/warehouse/product/exist', 'WareHouseController@productExist');
-    Route::post('/warehouse/product/add', 'WareHouseController@addProduct');
-    Route::post('/warehouse/product/update', 'WareHouseController@updateProduct');
-    Route::get('/warehouse/{id}/products/add', 'WareHouseController@newProduct');
-    Route::get('/warehouse/{wh_id}/products/delete/{id}', 'WareHouseController@deleteProduct');
-    Route::get('/warehouse/{id}/products/edit/{p_id}', 'WareHouseController@editProduct');
+        Route::get('/warehouse/{id}/products', 'WareHouseController@index');
+        Route::get('/warehouse/edit/{id}', 'WareHouseController@editWareHouse');
+        Route::post('/warehouse/update', 'WareHouseController@updateWarehouse');
+        Route::get('/warehouse/product/exist', 'WareHouseController@productExist');
+        Route::post('/warehouse/product/add', 'WareHouseController@addProduct');
+        Route::post('/warehouse/product/update', 'WareHouseController@updateProduct');
+        Route::get('/warehouse/{id}/products/add', 'WareHouseController@newProduct');
+        Route::get('/warehouse/{wh_id}/products/delete/{id}', 'WareHouseController@deleteProduct');
+        Route::get('/warehouse/{id}/products/edit/{p_id}', 'WareHouseController@editProduct');
 
-    Route::post('/warehouse/checkout/transaction', 'CheckoutController@checkout');
-    Route::get('/warehouse/checkout/{id}', 'CheckoutController@index');
-    Route::get('/warehouse/checkout/{id}/products', 'CheckoutController@getProducts');
-    Route::get('/warehouse/checkout/product/details', 'CheckoutController@getProductDetails');
-    Route::get('/warehouse/checkout/product/quantity', 'CheckoutController@validateQuantity');
+        Route::post('/warehouse/checkout/transaction', 'CheckoutController@checkout');
+        Route::get('/warehouse/checkout/{id}', 'CheckoutController@index');
+        Route::get('/warehouse/checkout/{id}/products', 'CheckoutController@getProducts');
+        Route::get('/warehouse/checkout/product/details', 'CheckoutController@getProductDetails');
+        Route::get('/warehouse/checkout/product/quantity', 'CheckoutController@validateQuantity');
 
-    Route::get('/warehouse/{id}/profit', 'ProfitController@index');
-    Route::get('/warehouse/{id}/profit/byName', 'ProfitController@getTransactionsByName');
-    Route::get('/warehouse/{id}/profit/byDate', 'ProfitController@getTransactionsByDate');
+        Route::get('/warehouse/{id}/profit', 'ProfitController@index');
+        Route::get('/warehouse/{id}/profit/byName', 'ProfitController@getTransactionsByName');
+        Route::get('/warehouse/{id}/profit/byDate', 'ProfitController@getTransactionsByDate');
 
-    Route::get('/warehouse/{id}/sales', 'SalesController@index');
-    Route::get('/warehouse/sales/byReceiptId', 'SalesController@getSales');
+        Route::get('/warehouse/{id}/sales', 'SalesController@index');
+        Route::get('/warehouse/sales/byReceiptId', 'SalesController@getSales');
 
-    Route::get('/warehouse/sales/byReceiptIdOrName', 'SalesController@getSalesByIdOrName');
+        Route::get('/warehouse/sales/byReceiptIdOrName', 'SalesController@getSalesByIdOrName');
 
-    Route::post('/warehouse/sales/delete', 'SalesController@deleteSales');
+        Route::post('/warehouse/sales/delete', 'SalesController@deleteSales');
 
-    Route::get('/print/receipt/{id}', 'PrinterController@receipt');
+        Route::get('/print/receipt/{id}', 'PrinterController@receipt');
+    });
 });
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -18,6 +18,22 @@
                     <li><a href="/"> <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"> </span>
                             Warehouses</a></li>
                 @endif
+
+                        <!-- Authentication Links -->
+                @if (Auth::guest())
+                    <li><a href="{{ url('/login') }}">Login</a></li>
+                    <li><a href="{{ url('/register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <span class="glyphicon glyphicon-user" aria-hidden="true"> </span> {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                        </ul>
+                    </li>
+                @endif
             </ul>
         </div>
     </div>
